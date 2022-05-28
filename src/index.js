473d05c4cdb1138a25f39db04162831234a0e6a4
Jean Junior <jean.gomes@tatic.net>
2022-05-27 23:31:49 -0300

    feat: graceful shutdown

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { handler } from './router/handler.js'
 
 const PORT = process.env.PORT || 3000
 
-export const server = http.createServer(handler).listen(PORT).once('listening', () => {
-  console.log(`Server running on port ${PORT}`)
-})
+export const server = http
+  .createServer(handler)
+  .listen(PORT)
+  .once('listening', () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 
 
 // Catch all errors
@@ -14,4 +17,19 @@ process.on('uncaughtException', (err, origin) => {
 })
 process.on('unhandledRejection', async (reason, promise) => {
   console.log(reason)
+})
+
+// Graceful shutdown
+function gracefulShutdown(event) {
+  console.log(`Received ${event} event`)
+  console.log('Shutting down...')
+  server.close(() => {
+    console.log('Shutdown complete')
+    process.exit(0)
+  })
+}
+process.on('SIGTERM', gracefulShutdown)
+process.on('SIGINT', gracefulShutdown)
+process.on('exit', (code) => {
+  console.log(`Exit signal received code ${code}`)
 })
